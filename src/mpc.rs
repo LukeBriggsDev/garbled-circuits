e57b1a0e92d7b5a8df332494ac5593d287b440b1
Luke Briggs <22104392+LukeBriggsDev@users.noreply.github.com>
2024-04-26 11:18:23 +0100

    Implement Oblivious Transfer

--- src/mpc.rs
+++ src/mpc.rs
@@ -1,63 +1,72 @@
 use aes_gcm::{AeadCore, AeadInPlace, Aes256Gcm, Key, KeyInit};
-use aes_gcm::aead::{Aead, OsRng};
+use aes_gcm::aead::{Aead, Nonce, OsRng};
 use garble_lang::circuit::Circuit;
-use pqc_kyber::{decapsulate, encapsulate, keypair, PublicKey};
+use kyber_pke;
+use kyber_pke::ct_len;
+use pqc_kyber::KYBER_PUBLICKEYBYTES;
+use rand::Rng;
 use crate::evaluator::evaluate;
 
 use crate::garbler::{garble_circuit};
-use crate::util::AESNoncePair;
+use crate::util::{AESNoncePair, xor_vec};
 
-// fn oblivious(garbler: &Garbler, evaluator: &mut Evaluator) {
-//     // Alice part a
-//     let keypairs = vec![keypair(&mut OsRng).unwrap(), keypair(&mut OsRng).unwrap()];
-//
-//     // Bob part b
-//     let public_keys:Vec<PublicKey> = keypairs.iter().map(|key| key.public).collect();
-//     let chosen_key = public_keys[evaluator.choice as usize];
-//     let (ciphertext, bob_key) = encapsulate(chosen_key.as_slice(), &mut OsRng).unwrap();
-//
-//     // Alice part b
-//     let symmetric_keys = keypairs.iter().map(|keypair| decapsulate(&ciphertext, keypair.secret.as_slice()).unwrap());
-//     let mut cipher_list = Vec::new();
-//     let mut nonce_pairs = Vec::new();
-//     for (idx, key) in symmetric_keys.enumerate() {
-//         let mut ciphertext = Vec::new();
-//         ciphertext.extend_from_slice(garbler.get_evaluator_keys().get(idx as u8).key.as_slice());
-//         ciphertext.extend_from_slice(garbler.get_evaluator_keys().get(idx as u8).nonce.as_slice());
-//         let aes_key: Key<Aes256Gcm> = key.into();
-//         let cipher = Aes256Gcm::new(&aes_key);
-//         let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
-//         let pair = AESNoncePair {
-//             key: aes_key,
-//             nonce: nonce
-//         };
-//         cipher.encrypt_in_place(&nonce, b"", &mut ciphertext);
-//         cipher_list.push(ciphertext);
-//         nonce_pairs.push(pair);
-//     }
-//
-//     // Bob part c
-//     let bob_key: &Key<Aes256Gcm> = &bob_key.into();
-//     let cipher_bob = Aes256Gcm::new(&bob_key);
-//
-//     for (pair, ciphertext) in nonce_pairs.iter().zip(cipher_list) {
-//         let decrypt = cipher_bob.decrypt(&pair.nonce, ciphertext.as_slice());
-//         match decrypt {
-//             Ok(message) => {
-//                 let aes: [u8; 32] = message[..32].try_into().expect("AES INCORRECT SIZE");
-//                 let nonce: [u8; 12] = message[32..].try_into().expect("NONCE INCORRECT SIZE");
-//                 let aes_pair = AESNoncePair {
-//                     key: aes.into(),
-//                     nonce: nonce.into(),
-//                 };
-//                 evaluator.set_choice_key(aes_pair);
-//             }
-//             Err(message) => {
-//                 println!("{}", message)
-//             }
-//         }
-//     }
-// }
+
+fn oblivious(messages: (Vec<u8>, Vec<u8>), choice: bool) {
+    // Receiver
+    let (pub_key, priv_key) = kyber_pke::pke_keypair().unwrap();
+
+    let nonce: [u8; 32] = OsRng.gen();
+    let nonce_pair = AESNoncePair::new();
+    let pair_bytes = [nonce_pair.key.as_slice(), nonce_pair.nonce.as_slice()].concat();
+    let c_bit = kyber_pke::encrypt(pub_key, pair_bytes, nonce).unwrap();
+    let mut c_other = Vec::new();
+    for _ in 0..c_bit.len()-8 {
+        c_other.push(OsRng.gen())
+    }
+    for i in c_bit.len()-8..c_bit.len() {
+        c_other.push(c_bit[i])
+    }
+
+    let mut enc = match choice {
+        false => {(c_bit, c_other)}
+        true => {(c_other, c_bit)}
+    };
+
+    // Sender
+    let s = vec![
+        kyber_pke::decrypt(priv_key, enc.0).unwrap(),
+        kyber_pke::decrypt(priv_key, enc.1).unwrap()
+    ];
+
+    let mut ret = Vec::new();
+    for (idx, plaintext) in s.iter().enumerate() {
+        let key = Key::<Aes256Gcm>::from_slice(&s[idx][..32]);
+        let nonce = Nonce::<Aes256Gcm>::from_slice(&s[idx][32..]);
+        let cipher = Aes256Gcm::new(&key);
+        match idx {
+            0 => {
+                ret.push(cipher.encrypt(&nonce, messages.0.as_ref()).unwrap())
+            }
+            1 => {
+                ret.push(cipher.encrypt(&nonce, messages.1.as_ref()).unwrap())
+            }
+            _ => {
+                panic!("TOO MANY MESSAGES")
+            }
+        }
+    }
+
+    // receiver
+    let mut result = Vec::new();
+    let cipher = Aes256Gcm::new(&nonce_pair.key);
+    for ciphertext in ret {
+        result.push(cipher.decrypt(&nonce_pair.nonce, ciphertext.as_ref()));
+
+    }
+
+    println!("{:?}", result);
+
+}
 
 pub trait OTEval {
     fn ot_eval(&self, inputs: &[Vec<bool>]) -> Vec<bool>;
@@ -65,13 +74,10 @@ pub trait OTEval {
 
 impl OTEval for Circuit {
     fn ot_eval(&self, inputs: &[Vec<bool>]) -> Vec<bool> {
+        oblivious(([0u8;32].to_vec(), [1u8;32].to_vec()), false);
         let garbled_circuit = garble_circuit(self);
         let result = evaluate(&garbled_circuit, inputs);
 
         result.iter().map(|&x| if x != 0 {true} else {false}).collect()
-        // evaluator.set_garbled_outputs(garbler.get_outputs().clone());
-        // evaluator.set_garble_key(garbler.get_choice_key());
-        // oblivious(&garbler, &mut evaluator);
-        // evaluator.decrypt_outputs();
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,15 @@
 use aes_gcm::{AeadCore, Aes256Gcm, Key, KeyInit};
 use aes_gcm::aead::{Nonce, OsRng};
 
+pub fn xor_vec(v1: Vec<u8>, v2: Vec<u8>) -> Vec<u8> {
+    let v3: Vec<u8> = v1
+        .iter()
+        .zip(v2.iter())
+        .map(|(&x1, &x2)| x1 ^ x2)
+        .collect();
+    return v3;
+}
+
 #[derive(Copy, Clone, Debug)]
 pub struct AESNoncePair {
     pub key: Key<Aes256Gcm>,
